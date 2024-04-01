717a168a0eb3885e939feab8f7180805d1b3fa30
Polo_1245 <81937184+polo-1245-oficial@users.noreply.github.com>
2024-04-01 20:47:35 +0200

    arrays 😯😯

--- src/software/tux/polo/Main.java
+++ src/software/tux/polo/Main.java
@@ -1,17 +1,25 @@
 package software.tux.polo;
 
-
 import java.util.Scanner;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] array = new String[]{
+            "primera", //0
+            "segunda",
+            "tercera",
+            "cuarta",
+            "quinta",
+            "séptima",
+            "octava",
+            "novena",
+            "décima",
+        };
         int numero = 0;
-
         int intentos = 0;
         int intentosMax = 100; //habrá otra manera pero idk y null no deja so
-        String intentostopalabra = "";
         //System.out.println(numeroaleatorio);
         System.out.println("El juego del número aleatorio.");
         while (intentosMax > 10){
@@ -19,8 +27,12 @@ public class Main {
             intentosMax = scanner.nextInt();
         }
 
-        System.out.println("¿Hastá qué numero quieres que genere el juego?");
-        numero = scanner.nextInt();
+        while (numero <= 1){
+            System.out.println("¿Hastá qué numero quieres que genere el juego?");
+            numero = scanner.nextInt();
+        }
+
+
         int numeroinfo = numero;
         int numeroaleatorio = ThreadLocalRandom.current().nextInt(1, numero); //generar el número aleatorio y definirlo
 
@@ -31,43 +43,12 @@ public class Main {
         }
 
         if(numero == numeroaleatorio) {
-            switch (intentos) { //para definir primera, segunda, tercera vez sistema "eficiente"
-                case 1:
-                    intentostopalabra = "primera";
-                break;
-                case 2:
-                    intentostopalabra = "segunda";
-                break;
-                case 3:
-                    intentostopalabra = "tercera";
-                break;
-                case 4:
-                    intentostopalabra = "cuarta";
-                    break;
-                case 5:
-                    intentostopalabra = "quinta";
-                    break;
-                case 6:
-                    intentostopalabra = "sexta";
-                    break;
-                case 7:
-                    intentostopalabra = "séptiva";
-                    break;
-                case 8:
-                    intentostopalabra = "octava";
-                    break;
-                case 9:
-                    intentostopalabra = "novena";
-                    break;
-                case 10:
-                    intentostopalabra = "décima";
-                    break;
-            }
-            System.out.println("Felicidades! Adivinaste el número aleatorio.\nEl número era: " + numeroaleatorio + " \nY lo adivinaste a la " + intentostopalabra + " vez.");
+
+            System.out.println("Felicidades! Adivinaste el número aleatorio.\nEl número era: " + numeroaleatorio + " \nY lo adivinaste a la " + array[intentos-1] + " vez."); //el -1 es porque el array empieza desde el 0 no desde el 1
             System.exit(0); //System.exit así saldrá sin que se ejecute el if de abajo, de esta manera si al tercer intento lo adivinaste te saldrá el felicidades no el intentos
         }
 
-        if(intentos >= 3){
+        if(intentos >= intentosMax){
             System.out.println("Has tenido "+ intentosMax +" intentos para comprobar el número pero no lo adivinaste.");
             System.exit(0);
         }
